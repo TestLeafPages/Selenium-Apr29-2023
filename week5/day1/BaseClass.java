2351951e320acb03885b9b1c17e58aeff5f9a35d
TestLeafPages <earth@testleaf.com>
2023-05-27 19:36:26 +0530

    Add files via upload

---
+++ week5/day1/BaseClass.java
@@ -0,0 +1,34 @@
+package week5.day1.testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+	
+	public static ChromeDriver driver;
+	
+	@BeforeTest
+	public void preCondition() {
+		
+		driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	
+	@AfterTest
+	public void postCondition() {
+	    driver.close();	
+	}
+
+}
---
+++ week5/day1/CreateLead.java
@@ -0,0 +1,30 @@
+package week5.day1.testcase;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass{
+
+	@Test
+	public void runCreateLead() {
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Subraja");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Subi");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		if (text.contains("Testleaf")) {
+			System.out.println("Lead created successfully");
+		}
+		else {
+			System.out.println("Lead is not created");
+		}
+		
+
+
+	}
+
+}
